bee15d00bbf48122944f1e605523847c120ae5ea
sevontheedge <sivthiago@hotmail.com>
2019-05-08 11:14:23 -0400

    testes imp

--- src/main.ml
+++ src/main.ml
@@ -8,6 +8,8 @@ open Pi;;
 open Printf;;
 
 let willReadFile = ref false;;
+let willPrintSourceCode = ref false;;
+let willPrintTree = ref false;;
 let fileContents = ref "";;
   
     
@@ -18,6 +20,12 @@ let () =
 
       if arg = "-f" then begin
         willReadFile := true;
+      end else if arg = "-a" then begin
+        willPrintTree := true;
+      end else if arg = "-t" then begin 
+        Automaton.willPrintStackTrace := true;
+      end else if arg = "-s" then begin 
+        willPrintSourceCode := true;
       end else begin
         if !willReadFile then begin 
           fileContents := readInputFile arg;
@@ -27,15 +35,21 @@ let () =
         end
       end
   done;
-  (* reclamar se nao tiver aberto o arquivo *)
-  print_endline !fileContents;
 
-  let tree = Parser.main Lexer.token (Lexing.from_string !fileContents) 
+  if !fileContents = "" then raise (Invalid_argument "Arquivo não inserido");
+
+  if !willPrintSourceCode then print_endline ("Código fonte:\n" ^ !fileContents  ^ "\n");
+
+  
+  let tree = Statement(Parser.main Lexer.token (Lexing.from_string !fileContents) )
   and controlStack = (Stack.create()) 
   and valueStack = (Stack.create()) 
   and environment = (Hashtbl.create 10)
   and memory = (Hashtbl.create 10)  in
-        (Stack.push (Statement(tree)) controlStack);
+
+        if !willPrintTree then print_endline ("Árvore Sintática:\n" ^ (string_of_ctn tree) ^ "\n");
+
+        (Stack.push tree controlStack);
         (*inicialização de y*)
         (Hashtbl.add environment "y" (Automaton.Loc(23)) );
         (Hashtbl.add  memory 23 (Automaton.Integer(19)) );
@@ -45,6 +59,9 @@ let () =
         (*inicialização de x *)
         (Hashtbl.add environment "x" (Automaton.Loc(24))  );
         (Hashtbl.add memory 24 (Automaton.Integer(13)) );
+        (*inicialização de i *)
+        (Hashtbl.add environment "i" (Automaton.Loc(456))  );
+        (Hashtbl.add memory 456 (Automaton.Integer(0)) );
         Automaton.evaluatePi controlStack valueStack environment memory;;
 
 
